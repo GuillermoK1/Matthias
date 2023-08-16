77f335f6910206376b1acefa6e42707354e6afb4
GuillermoK1 <guillermojkr@gmail.com>
2023-08-16 10:42:54 +0100

    get,run,div,exp15k

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import os
+import shutil
+from archivo_py import funciones
 
 files = []
-
 archivo_import = os.getcwd() + '/import'
+archivo_export = os.getcwd() + '/export'
+
+# Crear la carpeta de exportación si no existe
+if not os.path.exists(archivo_export):
+    os.makedirs(archivo_export)
 
 for file in os.listdir(archivo_import):
-    print(file)
     if file.endswith('.csv'):
         files.append(file)
 
+if len(files) == 0:
+    print('No hay archivos CSV en la carpeta de importación.')
 
-while True:
-    if len(files) == 0:
-        print('no hay files')
-        break
-
-    for file in files:
-        print('ahora hago algo con', file)
-
+file_num = 1
+for file in files:
+    # Realizar las funciones del archivo.py en el archivo CSV
+    result = funciones(os.path.join(archivo_import, file))
+    
+    # Dividir el resultado en partes de 15000 filas
+    result_parts = [result[i:i+15000] for i in range(0, len(result), 15000)]
+    
+    # Exportar cada parte a un archivo separado
+    for part in result_parts:
+        export_file = os.path.join(archivo_export, f'resultado_{file_num}.csv')
+        with open(export_file, 'w') as f:
+            for row in part:
+                f.write(','.join(row) + '\n')
+        file_num += 1
