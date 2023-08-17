e9b336e561db4b036d9c41b88872759befdc745c
GuillermoK1 <139216670+GuillermoK1@users.noreply.github.com>
2023-08-17 21:08:09 +0100

    Update main.py
    This script is a Python program that processes CSV files in a specific directory. Here's what it does:
    
    1. It imports the necessary modules: `os`, `shutil`, and `funciones` from `archivo_py`.
    2. It creates two variables, `archivo_import` and `archivo_export`, which store the paths to the import and export directories, respectively.
    3. It checks if the export directory exists, and if it doesn't, it creates it using the `os.makedirs()` function.
    4. It then lists all the files in the import directory using the `os.listdir()` function and filters out only the files that end with `.csv` using the `str.endswith()` method.
    5. If there are no CSV files in the import directory, it prints a message saying so.
    6. Otherwise, for each CSV file, it calls the `funciones()` function (imported from `archivo_py`) with the file path as an argument and stores the result in a variable called `result`.
    7. The result is then split into parts of 15000 rows each using list slicing.
    8. For each part, a new CSV file is created in the export directory with a name in the format `resultado_{file_num}.csv`, where `{file_num}` is replaced by the current file number.
    9. The rows in each part are then written to the new CSV file using a `for` loop and the `str.join()` method to join the values in each row with a  ';  ' separator.
    10. Finally, the file number is incremented by 1.
    
    This script processes CSV files in batches of 1499 rows and exports them to new CSV files, keeping their column names in the first row, in a specified directory.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import os
-#import shutil
 from archivo_py import funciones
-'''
-Mirar carpeta_import y carpeta_export.
-'''
 files = []
 archivo_import = os.getcwd() + '/import'
 archivo_export = os.getcwd() + '/export'
 
-# Crear la carpeta de exportación si no existe
 if not os.path.exists(archivo_export):
     os.makedirs(archivo_export)
 
@@ -21,17 +16,19 @@ if len(files) == 0:
 
 file_num = 1
 for file in files:
-    # Realizar las funciones del archivo.py en el archivo CSV
     result = funciones(os.path.join(archivo_import, file))
     
-    # Dividir el resultado en partes de 15000 filas
-    '''
-    result_parts = [result[i:i+15000] for i in range(0, len(result), 15000)]
+    # Get the column names from the first row of the result
+    column_names = result[0]
     
-    # Exportar cada parte a un archivo separado
+    result_parts = [result[i:i+14999] for i in range(0, len(result), 14999)]
+    
     for part in result_parts:
         export_file = os.path.join(archivo_export, f'resultado_{file_num}.csv')
         with open(export_file, 'w') as f:
+        
+            f.write(','.join(column_names) + '\n')
+            
             for row in part:
                 f.write(','.join(row) + '\n')
         file_num += 1
